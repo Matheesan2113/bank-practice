365706be082fac12f815183ba0a9a68df8197c1a
mathe <mathe@ASUS-DESKTOP>
2016-11-24 02:13:07 -0500

    adding to git

---
+++ src/bank/Account.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author m2manoka
+ */
+interface Account {
+
+    void add(int x);
+
+    void sub(int x);
+
+    String toString();
+
+    int getValue();
+}
